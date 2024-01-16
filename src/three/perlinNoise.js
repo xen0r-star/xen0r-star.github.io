c14b8379c2c4a2f1f890c1bd72cfe94cb998a024
Florian <florian241006@gmail.com>
2024-01-16 21:05:38 +0100

    change comment perlin noise

--- src/three/perlinNoise.js
+++ src/three/perlinNoise.js
@@ -1,14 +1,14 @@
 let grid = [];
 const nodes = 4;
 
-// Fonction permettant de générer un vecteur unitaire aléatoire
-function random_unit_vector() {
+
+function random_unit_vector() { // Genere vecteur unitaire aléatoire
     let theta = Math.random() * 2 * Math.PI;
     return { x: Math.cos(theta), y: Math.sin(theta) };
 }
 
-// Populate the grid with random unit vectors
-for (let i = 0; i < nodes; i++) {
+
+for (let i = 0; i < nodes; i++) { // Remplire grid
     let row = [];
     for (let j = 0; j < nodes; j++) {
         row.push(random_unit_vector());
@@ -16,59 +16,58 @@ for (let i = 0; i < nodes; i++) {
     grid.push(row);
 }
 
-// Function to calculate dot product between distance vector and gradient vector
-function dot_prod_grid(x, y, vert_x, vert_y) {
+
+function dot_prod_grid(x, y, vert_x, vert_y) { // Calcule produit point entre vecteur distance && vecteur gradient
     var g_vect = grid[vert_y][vert_x];
     var d_vect = { x: x - vert_x, y: y - vert_y };
     return d_vect.x * g_vect.x + d_vect.y * g_vect.y;
 }
 
-// Smootherstep function for interpolation
-function smootherstep(x) {
+
+function smootherstep(x) { // Fonction Smootherstep pour l'interpolation
     return 6 * x ** 5 - 15 * x ** 4 + 10 * x ** 3;
 }
 
-// Function for linear interpolation with smootherstep
-function interp(x, a, b) {
+
+function interp(x, a, b) { // Fonction d'interpolation linéaire avec smootherstep
     return a + smootherstep(x) * (b - a);
 }
 
-// Perlin noise function
-function perlin_get(x, y) {
-    // Determine cell nodes
+
+function perlin_get(x, y) { // Bruit de perlin
+    // Determiner les nœuds des cellules
     let x0 = Math.floor(x);
     let x1 = x0 + 1;
     let y0 = Math.floor(y);
     let y1 = y0 + 1;
 
-    // Calculate dot products and interpolate
+    // Calculer les produits points et interpoler
     let dot00 = dot_prod_grid(x, y, x0, y0);
     let dot01 = dot_prod_grid(x, y, x0, y1);
     let dot10 = dot_prod_grid(x, y, x1, y0);
     let dot11 = dot_prod_grid(x, y, x1, y1);
 
-    // Interpolate along x-axis
+    // Interpoler le long de l'axe des x
     let interp0 = interp(x - x0, dot00, dot10);
     let interp1 = interp(x - x0, dot01, dot11);
 
-    // Interpolate along y-axis
+    // Interpoler le long de l'axe des y
     return interp(y - y0, interp0, interp1);
 }
 
 
 export function Noise(gridSizeX, gridSizeY) {
-    // Créer une matrice pour stocker les valeurs du bruit de Perlin
-    let perlinGrid = [];
+    let perlinGrid = []; // Cree matrice
     
-    // Remplir la matrice avec les valeurs du bruit de Perlin
-    for (let i = 0; i < gridSizeX; i++) {
+    for (let i = 0; i < gridSizeX; i++) { // Remplie matrice
         let row = [];
         for (let j = 0; j < gridSizeY; j++) {
-            // Calculer les coordonnées normalisées entre 0 et 1
+
+            // Calculer les coordonnees normalisees entre 0 et 1
             const x = i / gridSizeX;
             const y = j / gridSizeY;
     
-            // Obtenir la valeur du bruit de Perlin pour les coordonnées continues
+            // Obtenir la valeur du bruit de Perlin pour les coordonnees continues
             const perlinValue = perlin_get(x, y);
     
             // Stocker la valeur dans la matrice
