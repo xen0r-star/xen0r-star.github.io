7cbaeda6a5a71689c8053de480703f36ce839d3b
Florian <florian241006@gmail.com>
2024-01-29 22:31:27 +0100

    update

--- src/three/object.js
+++ src/three/object.js
@@ -1,10 +1,12 @@
 let link = 'https://raw.githubusercontent.com/xen0r-star/xen0r-star.github.io/main'
 
 // if (process.env.variable1 == false) {
-    let ship = link + '/public/assets/Ship.gltf';
-// } else {
-    // import ship from '@public/assets/Ship.gltf';
-// }
+    // let ship = link + '/public/assets/Ship.gltf';
+    // } else {
+        // import ship from '@public/assets/Ship.gltf';
+        // }
+        
+let ship = link + '/public/assets/Ship.gltf';
 
 const assets = {
     ship: ship,
